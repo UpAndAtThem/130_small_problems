f72c19baf7aa2359cc98d597c56fb5664c4d860d
Jason Nelson <Jasonn318@gmail.com>
2018-10-30 15:00:59 -0500

    add count.rb

---
+++ easy_1/count.rb
@@ -0,0 +1,13 @@
+def count(arr)
+  count = 0
+
+  arr.each { |item| count += 1 if yield(item)}
+  count
+end
+
+count([1,2,3,4,5]) { |value| value.odd? } == 3
+count([1,2,3,4,5]) { |value| value % 3 == 1 } == 2
+count([1,2,3,4,5]) { |value| true } == 5
+count([1,2,3,4,5]) { |value| false } == 0
+count([]) { |value| value.even? } == 0
+count(%w(Four score and seven)) { |value| value.size == 5 } == 2
