1b5e28bf2798027d65a4fe37274470ce222433b2
Jason Nelson <Jasonn318@gmail.com>
2018-10-30 13:32:47 -0500

    refactored instance methods into class

--- easy_1/rot13.rb
+++ easy_1/rot13.rb
@@ -5,17 +5,47 @@ class Cryptograph
   DECRYPTOR = KEYS.zip(VALUES).to_h
   ENCRYPTOR = VALUES.zip(KEYS).to_h
 
-  def decipher(encoded_message)
-    encoded_message.chars.map{ |letter| DECRYPTOR[letter]}.join
+  def self.decrypt(encrypted_message)
+    result = encrypted_message.chars.map do |letter| 
+      DECRYPTOR.keys.include?(letter) ? DECRYPTOR[letter] : letter
+    end
+
+    result.join
   end
 
-  def encrypt(message)
-    message.chars.map{ |letter| ENCRYPTOR[letter]}.join
+  def self.encrypt(message)
+    result = message.chars.map do |letter| 
+      ENCRYPTOR.keys.include?(letter) ? ENCRYPTOR[letter] : letter
+    end
+
+    result.join
+  end
+
+  def self.decrypt_list(list)
+    list.map { |message| Cryptograph.decrypt message }
   end
 end
 
-cryptograph = Cryptograph.new
+programming_greats = ['Nqn Ybirynpr',
+                      'Tenpr Ubccre',
+                      'Nqryr Tbyqfgvar',
+                      'Nyna Ghevat',
+                      'Puneyrf Onoontr',
+                      'Noqhyynu Zhunzznq ova Zhfn ny-Xujnevmzv',
+                      'Wbua Ngnanfbss',
+                      'Ybvf Unvog',
+                      'Pynhqr Funaaba',
+                      'Fgrir Wbof',
+                      'Ovyy Tngrf',
+                      'Gvz Orearef-Yrr',
+                      'Fgrir Jbmavnx',
+                      'Xbaenq Mhfr',
+                      'Fve Nagbal Ubner',
+                      'Zneiva Zvafxl',
+                      'Lhxvuveb Zngfhzbgb',
+                      'Unllvz Fybavzfxv',
+                      'Tregehqr Oynapu']
 
-cryptograph.encrypt "HELLO WORLD" # => "URYYB JBEYQ"
-
+puts Cryptograph.decrypt 'Nyna Ghevat' # => "Alan Turing"
+puts Cryptograph.decrypt_list(programming_greats) # returns decrypted array
 
